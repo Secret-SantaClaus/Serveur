a413c355b8d132ed7d6cb53f523b2d669fafe90a
Secret-SantaClaus <noel.secret.santaclaus@gmail.com>
2025-01-19 12:19:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ http
       "Access-Control-Allow-Origin",
       "https://secret-santaclaus.github.io"
     );
+    let value
+    let nom
+    let mail
     let data = req.url.split("?")
-    data.split("&")
-    nom = data[0].split("=")
+    data[1].split("&")
+    value = data[1][0].split("=")
+    nom = value[1]
     nom.shift()
     nom.pop()
+    nom.split(",")
+    value = data[1][0].spli("=")
+    mail = value[1]
     mail = data[1].split("=")
     mail.shift()
     mail.pop()
-    nom.split(",")
     mail.split(",")
 
     let personne1 = Math.floor(Math.random() * (nom.length - 1));
