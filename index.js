b52b8c207480f2c7271025022f3d6d0275789ecd
Secret-SantaClaus <noel.secret.santaclaus@gmail.com>
2025-01-19 12:28:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,39 +9,39 @@ http
       "https://secret-santaclaus.github.io"
     );
     if (req.url +! "/") {
-    let value
-    let nom
-    let mail
-    let data = req.url
-    data.split("?")
-    data[1].split("&")
-    value = data[1][0].split("=")
-    nom = value[1]
-    nom.shift()
-    nom.pop()
-    nom.split(",")
-    value = data[1][0].spli("=")
-    mail = value[1]
-    mail = data[1].split("=")
-    mail.shift()
-    mail.pop()
-    mail.split(",")
+      let value
+      let nom
+      let mail
+      let data = req.url
+      data.split("?")
+      data[1].split("&")
+      value = data[1][0].split("=")
+      nom = value[1]
+      nom.shift()
+      nom.pop()
+      nom.split(",")
+      value = data[1][0].spli("=")
+      mail = value[1]
+      mail = data[1].split("=")
+      mail.shift()
+      mail.pop()
+      mail.split(",")
 
-    let personne1 = Math.floor(Math.random() * (nom.length - 1));
-    let NomPers1 = nom[personne1];
-    let destinataire = personne1;
-    let envoyeur;
-    let longueur = nom.length - 1;
+      let personne1 = Math.floor(Math.random() * (nom.length - 1));
+      let NomPers1 = nom[personne1];
+      let destinataire = personne1;
+      let envoyeur;
+      let longueur = nom.length - 1;
 
-    for (let i = 0; i < longueur; i++) {
-      envoyeur = mail[destinataire];
-      nom.splice(destinataire, 1);
-      mail.splice(destinataire, 1);
-      let long = nom.length - 1;
-      destinataire = Math.floor(Math.random() * long);
-      envoyerMail(envoyeur, nom[destinataire]);
-    }
-    envoyerMail(mail[0], NomPers1);
+      for (let i = 0; i < longueur; i++) {
+        envoyeur = mail[destinataire];
+        nom.splice(destinataire, 1);
+        mail.splice(destinataire, 1);
+        let long = nom.length - 1;
+        destinataire = Math.floor(Math.random() * long);
+        envoyerMail(envoyeur, nom[destinataire]);
+      }
+      envoyerMail(mail[0], NomPers1);
     }
     res.write(req.url);
     res.end(etat);
