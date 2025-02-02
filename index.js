db154993a5fcd73adc4ae2d459e13f6566a60911
Secret-SantaClaus <noel.secret.santaclaus@gmail.com>
2025-02-02 19:33:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ http
       let data = req.url.split("&");
       value = data[0].split("=");
       nom = value[1];
-      nom = nom.substring(3, nom.length -3);
+      nom = nom.substring(3, nom.length -7);
       nom = nom.split("%2C");
       value = data[1].split("=");
       mail = value[1];
-      mail = mail.substring(3, mail.length -3);
+      mail = mail.substring(3, mail.length -7);
       mail = mail.split("%2C");
       console.log(nom);
       console.log(mail);
